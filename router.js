f3569ddeb03ec2e4ebd887909cbdd07268585454
enlore <n.e.lorenson@gmail.com>
2016-06-20 21:57:49 -0500

    Fire callbacks keyed to strings

--- router.js
+++ router.js
@@ -2,7 +2,15 @@
 
 class Router {
     constructor () {
+        this.routes = {};
+    }
 
+    setHandler (route, cb) {
+        this.routes[route] = cb;
+    }
+
+    handle (route) {
+        this.routes[route]();
     }
 }
 
--- test/router.js
+++ test/router.js
@@ -1,8 +1,21 @@
 "use strict";
+require("should");
 
+const sinon = require("sinon");
 const Router = require("../router");
 const r = new Router();
 
 describe("Router", function () {
-    it("fires correct callback on matched route string");
+    it("fires correct callback on matched route string", doRoute);
 });
+
+function doRoute (done) {
+    let cb = sinon.spy(function () {
+        cb.calledOnce.should.be.true();
+        done();
+    });
+
+    r.setHandler("route-string", cb);
+
+    r.handle("route-string");
+}
