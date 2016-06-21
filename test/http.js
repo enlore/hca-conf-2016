864949c3d3e43dac6613f4398f6531cd04f2edba
enlore <n.e.lorenson@gmail.com>
2016-06-20 21:59:53 -0500

    Add second route test /hello

--- test/http.js
+++ test/http.js
@@ -2,9 +2,15 @@ const api = require("./api");
 
 describe("api speaks http", function () {
     it("responds to GET /", doGet);
+    it("responds to GET /api/hello", doHello);
 })
 
 function doGet (done) {
     api.get("/")
     .expect(200, done)
+}
+
+function doHello (done) {
+    api.get("/api/hello")
+    .expect(200, done);
 }
