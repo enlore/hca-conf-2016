92fb888b3b68ca7803743a2079c94e4d33719733
enlore <n.e.lorenson@gmail.com>
2016-06-20 22:11:51 -0500

    Test for hello bob

--- test/http.js
+++ test/http.js
@@ -3,6 +3,7 @@ const api = require("./api");
 describe("api speaks http", function () {
     it("responds to GET /", doGet);
     it("responds to GET /api/hello", doHello);
+    it("responds to GET /api/not-found with 404", doNotFound);
 })
 
 function doGet (done) {
@@ -11,6 +12,14 @@ function doGet (done) {
 }
 
 function doHello (done) {
-    api.get("/api/hello")
+    api.get("/api/hello?name=bob")
+        .expect(function (res) {
+            res.text.should.equal("Hi bob");
+        })
     .expect(200, done);
+}
+
+function doNotFound (done) {
+    api.get("/api/not-found")
+    .expect(404, done);
 }
